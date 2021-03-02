996b5284459ad80b656f02af3087793047195a6c
yfuks <yoann.fuks@kiute.com>
2021-03-02 12:01:29 +0100

    add material ui

--- monkeys/src/App.tsx
+++ monkeys/src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
-import logo from './logo.svg';
 
 import Header from './components/header';
 import Home from './pages/home';
--- monkeys/src/components/header.tsx
+++ monkeys/src/components/header.tsx
@@ -1,19 +1,14 @@
 import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Button } from '@material-ui/core';
 
 const Header = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/macaque">macaque</Link>
-      </li>
-      <li>
-        <Link to="/babouin">babouin</Link>
-      </li>
-    </ul>
-  </nav>
+    <AppBar position="static">
+      <Toolbar>
+        <Button color="inherit"><Link to="/">Home</Link></Button>
+        <Button color="inherit"><Link to="/macaque">macaque</Link></Button>
+        <Button color="inherit"><Link to="/babouin">babouin</Link></Button>
+        </Toolbar>
+    </AppBar>
 );
 
 export default Header;
